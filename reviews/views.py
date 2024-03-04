dbb3b22b95d580c3a736a6277c8fa82e40299e20
matmaslanka <mates94@gmail.com>
2024-03-04 22:15:33 +0100

    passing data to database

--- reviews/views.py
+++ reviews/views.py
@@ -2,24 +2,30 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 from .forms import ReviewForm
+from . models import Review
 
 # Create your views here.
+
 
 def review(request):
     if request.method == 'POST':
         form = ReviewForm(request.POST)
 
-        if form.is_valid():                         #It checks is valid not empty.
-            print(form.cleaned_data)
+        if form.is_valid():  # It checks is valid not empty.
+            review = Review(
+                user_name=form.cleaned_data['user_name'], 
+                review_text=form.cleaned_data['review_text'], 
+                rating=form.cleaned_data['rating'])
+            review.save()
             return HttpResponseRedirect("/thank-you")
-        
-    else:    
+
+    else:
         form = ReviewForm()
 
     return render(request, "reviews/review.html", {
-    "form": form
+        "form": form
     })
-    
+
 
 def thank_you(request):
     return render(request, "reviews/thank_you.html")
