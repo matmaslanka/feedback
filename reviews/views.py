5225d7d79e64a3b63941587a9a072b584a171a0f
matmaslanka <mates94@gmail.com>
2024-03-07 21:02:43 +0100

    making three new classes

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,9 @@
+from typing import Any
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
+from django.views.generic.base import TemplateView
+from django.views.generic import ListView
 
 from .forms import ReviewForm
 from . models import Review
@@ -27,5 +30,39 @@ class ReviewView(View):
         })
 
 
-def thank_you(request):
-    return render(request, "reviews/thank_you.html")
+# class ThankYouView(View):
+#     def get(self, request):
+#         return render(request, "reviews/thank_you.html")
+
+
+class ThankYouView(TemplateView):
+    template_name = "reviews/thank_you.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['message'] = 'This works!'
+        return context
+    
+
+class ReviewsListView(ListView):
+    template_name = "reviews/review_list.html"
+    model = Review
+    context_object_name = "reviews"
+
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        reviews = Review.objects.all()
+        context['reviews'] = reviews
+        return context
+    
+
+class SingleReviewView(TemplateView):
+    template_name = "reviews/single_review.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        review_id = kwargs['id']
+        selected_review = Review.objects.get(pk=review_id)
+        context['review'] = selected_review
+        return context
